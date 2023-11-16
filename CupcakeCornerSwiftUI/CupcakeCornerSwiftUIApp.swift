ba6ddb3979b5e249b56b468c3b023fd7789f600d
ram4ik <ram4ik@hotmail.com>
2023-11-16 16:05:57 +0200

    Initial Commit

---
+++ CupcakeCornerSwiftUI/CupcakeCornerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerSwiftUIApp.swift
+//  CupcakeCornerSwiftUI
+//
+//  Created by admin on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
